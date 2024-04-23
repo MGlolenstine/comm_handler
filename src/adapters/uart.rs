30d0356d166005050343d65a9ccccd0d8de0a7cb
MGlolenstine <mglolenstine@gmail.com>
2024-04-23 13:33:03 +0200

    Added newline escape sequence for UART adapter.

--- src/adapters/uart.rs
+++ src/adapters/uart.rs
@@ -2,7 +2,8 @@ use crate::Result;
 use std::{sync::Arc, time::Duration};
 
 use log::{debug, trace};
-use serial2::{CharSize, FlowControl, IntoSettings, Parity, SerialPort, StopBits};
+use serial2::{IntoSettings, SerialPort};
+pub use serial2::{CharSize, StopBits, Parity, FlowControl};
 
 use crate::traits::{CloneableCommunication, Communication, CommunicationBuilder};
 
@@ -61,6 +62,7 @@ impl CommunicationBuilder for UartAdapterConfiguration {
         port.set_read_timeout(read_timeout)?;
         Ok(Box::new(UartAdapter {
             port: Arc::new(port),
+            buf: vec![],
         }))
     }
 }
@@ -68,6 +70,7 @@ impl CommunicationBuilder for UartAdapterConfiguration {
 #[derive(Clone)]
 pub struct UartAdapter {
     port: Arc<serial2::SerialPort>,
+    buf: Vec<u8>,
 }
 
 impl Communication for UartAdapter {
@@ -81,25 +84,46 @@ impl Communication for UartAdapter {
 
     fn recv(&mut self) -> Result<Option<Vec<u8>>> {
         self.error_if_not_connected()?;
-        let mut buf = vec![];
 
         // Would default of 1024 fit in well?
         let mut data = vec![0u8; 1024];
+
+        let does_buffer_contain_newline = |c: u8| c == b'\n' || c == b'\r';
+        let index_of_newline =
+            |buf: &[u8]| buf.iter().cloned().position(does_buffer_contain_newline);
 
         trace!("Reading");
         while let Ok(a) = self.port.read(&mut data) {
             if a == 0 {
                 break;
             }
-            buf.extend_from_slice(&data[0..a]);
+            self.buf.extend_from_slice(&data[0..a]);
+            if index_of_newline(&self.buf).is_some() {
+                break;
+            }
         }
-        trace!("Read {} bytes!", buf.len());
 
-        if buf.is_empty() {
-            Ok(None)
-        } else {
-            Ok(Some(buf))
+        // Only return data before the newline.
+        if let Some(index) = index_of_newline(&self.buf) {
+            let buf = self.buf.drain(..index).collect::<Vec<u8>>();
+            trace!("Read {} bytes!", buf.len());
+            while let Some(c) = self.buf.first() {
+                if (c == &b'\r') || (c == &b'\n') {
+                    self.buf.remove(0);
+                } else {
+                    break;
+                }
+            }
+            if buf.is_empty() {
+                return Ok(None);
+            }else{
+                return Ok(Some(buf));
+            }
         }
+
+        // No new line found
+        trace!("No newline found!");
+        Ok(None)
     }
 }
 
